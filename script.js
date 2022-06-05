1a04294d7f3d082909830fe8a4438ce61e999596
Michael Sullivan <104106828+ms13380@users.noreply.github.com>
2022-06-04 23:18:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
-var NINEAM = document.getElementById("9")
+var TENAM = document.getElementById("10")
+var ELEVENAM = document.getElementById("11")
+var TWELVEPM = document.getElementById("12")
+var ONEPM = document.getElementById("1")
+var TWOPM = document.getElementById("2")
+var THREEPM = document.getElementById("3")
+var FOURPM = document.getElementById("4")
+var FIVEPM = document.getElementById("5")
 
-// Get item from local storage if any -- DOESN'T WORK!!siojfioja
+// Get item from schedule.txt- DOESN'T WORK!!siojfioja
 var data = readFileSync("schedule.txt", 'utf-8').split("\n")
 
-//call vars to html
+//call vars to html for use
 NINEAM.innerHTML = "<p>" + data[0] + "</p>";
 
-// Display today's day and date
+// Display today's information
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-//make lots of edits, the save isn't working now??
-
 $(document).ready(function () {
-    // saveBtn click listener 
+    // saveBtn clk listner
     $(".saveBtn").on("click", function () {
         // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save schedule in text document
+        // Save information in txt document
         localStorage.setItem(time, text);
     })
    
